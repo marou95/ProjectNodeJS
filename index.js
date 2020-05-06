b47b8afcbd1e7d83194371d0e1f87931b930ab35
Jeremy Breant <breant.jeremy@yahoo.com>
2020-05-06 11:05:08 +0200

    maquette -> ejs + root + css + js

--- index.js
+++ index.js
@@ -7,6 +7,68 @@ app.get('/', function(req, res) {
     res.render("index.ejs");
 });
 
+app.get('/sign-up', function(req, res) {
+
+    res.render("signUp.ejs");
+});
+
+app.get('/my-projects', function(req, res) {
+
+    res.render("myProjects.ejs");
+});
+
+app.get('/tasks', function(req, res) {
+
+    res.render("tasks.ejs");
+});
+
+app.get('/new-project', function(req, res) {
+
+    res.render("newProject.ejs");
+});
+
+app.get('/task-detail', function(req, res) {
+
+    res.render("taskDetail.ejs");
+});
+
+app.get('/conversation', function(req, res) {
+
+    res.render("conversation.ejs");
+});
+
+app.get('/Git-commit', function(req, res) {
+
+    res.render("gitCommit.ejs");
+});
+
+
+
+
+// route pour jquery et bootstrap
+
+app.get('/vendor/bootstrap/css/bootstrap.min.css', function(req, res) {
+
+    res.sendFile("vendor/bootstrap/css/bootstrap.min.css", {root : '.'});
+});
+
+app.get('/css/simple-sidebar.css', function(req, res) {
+
+	res.sendFile("css/simple-sidebar.css", {root : '.'});
+});
+
+app.get('/vendor/jquery/jquery.min.js', function(req, res) {
+
+	res.sendFile("vendor/jquery/jquery.min.js", {root : '.'});
+});
+
+app.get('/vendor/bootstrap/js/bootstrap.bundle.min.js', function(req, res) {
+
+	res.sendFile("vendor/bootstrap/js/bootstrap.bundle.min.js", {root : '.'});
+});
+
+
+
 
 // on defini le port sur lequel on ecoute
 //server.listen(8080)
