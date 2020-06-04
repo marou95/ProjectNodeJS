c7b79f44e553ca902d2b76dfd19a1cc1f07d85af
Jeremy Breant <breant.jeremy@yahoo.com>
2020-06-04 19:27:22 +0200

    merge des 2 développement

--- index.js
+++ index.js
@@ -4,30 +4,90 @@ var app=express();
 var bodyParser=require("body-parser");
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+ 
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const Cookies = require('cookies');
 
+ //package de hashage des mdp
+var passwordHash = require('password-hash');
+var mongo = require('mongodb');
+var MongoClient = mongo.MongoClient;
+var JSONGit;
+ 
 // EN DUR POUR VERIFIER LE FONCTIONNEMENT
 var NAME = "letest";
 
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/Projet-L3";
 
+var rp = require('request-promise');
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//appel des derniers commits Github
+  
+
+// fonction middleware d'authentification: verification du token user
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        const privateKey = fs.readFileSync('private.key'); //lecture du fichier de la clé privé
+
+        jwt.verify(token, privateKey, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
 
 
 app.post('/sign_up', function(req,res){ 
-    var name = req.body.name;
+    name = req.body.name;
     var password = req.body.password; 
     var email =req.body.email;
     
     var hashedPassword = passwordHash.generate(password);
-    console.log(hashedPassword);
+    //console.log(hashedPassword); //mdp hashé
 
     //objet data 
     var data = { 
         "name": name, 
-        "password":hashedPassword, // Hasher le MDP 
+        "password":hashedPassword, 
         "email":email
     }
+
+//Ajout utilisateur en base via le signup form
+MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) { 
+    if (err) throw err;
+    var dbo = db.db("Projet-Trello");
+
+    //recherche dans la base si le username existe deja
+    dbo.collection('users').findOne({name : name}, function(err, result) {
+        if(err) throw err;
+        if(result){ //Si le username existe deja..
+            console.log("Found: "+name);
+            res.send('<script>alert("This username is already taken.")</script>');
+           // res.redirect('/signup');
+        }  
+        else{ //Sinon l'ajouter à la bdd
+            console.log("Not found: "+name);
+            dbo.collection('users').insertOne(data,function(err, collection){ 
+                res.redirect('/');
+                db.close();
+            });
+               
+        } 
+    
 
     MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) { 
         if (err) throw err;
@@ -42,7 +102,9 @@ app.post('/sign_up', function(req,res){
         });
     });
     
+    }); 
 }); 
+});
 
 app.get('/signup_success', function(req, res) {
 
@@ -84,7 +146,55 @@ app.get('/project/:name', function(req, res) {
                 res.render("project.ejs",{project: project, projectNameList: projectNameList});
             });
         }
+   });	
+});
+
+app.post('/connexion', function (req, res) {
+    console.log('connexion called');
+    var password = req.body.password;
+    var name = req.body.name;
+
+    // check si les données sont dans la requête post
+    if (!password || !name) {
+        res.send(undefined);
+    }
+    else {
+        //connexion a la db 
+        MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) { 
+            if (err) throw err;
+            var dbo = db.db("Projet-Trello");
+        dbo.collection('users').findOne({ name: name }, function (err, user) {  //requete pour trouver le user par son name
+            if (user) {  // si on trouve un user ET si le le mdp correcpond au hash en base
+                    if (passwordHash.verify(password, user.password)) {
+                        //res.send(user);
+                        var privateKey = fs.readFileSync('private.key');
+                        console.log(privateKey);
+                        var token = jwt.sign(user, privateKey,{ expiresIn: '24h' }); //
+                        var decoded = jwt.verify(token, privateKey);
+                        var cookies = new Cookies(req, res)
+                        var now = new Date()
+                        cookies.set('token', token, {expires: now.setTime(now.getTime+(24*60*60*1000) )})  
+                        console.log(decoded);
+                        res.redirect('/my-projects'/*?token=' + token + "&id=" + user._id*/ )
+                    }
+                    
+                    else {
+                        console.log('1');
+                        //res.redirect('/sign-up');
+                        //res.send(undefinded);
+                        
+                    }
+                
+            } else {
+                console.log('2');
+                //res.redirect('/sign-up');
+                //res.send(undefinded);
+                
+            }
+        })
     });
+    }
+    
 });
 
 //sauvegarde un projet (nouveau ou pas), champs : titre , personList
@@ -187,12 +297,51 @@ app.post('/save_task/:name/:task', function(req,res){
 app.get('/conversation/:name', function(req, res) {
     request.getProject(req.params.name).then((project) => {
         res.render("conversation.ejs",{project: project, userName: NAME});
+        
     });
 });
 
 app.get('/Git-commit/:name', function(req, res) {
     request.getProject(req.params.name).then((project) => {
-        res.render("gitCommit.ejs",{project: project});
+ 
+        var authorName;
+        var authorEmail;
+        var dateCommit;
+        var commitMessage;
+        
+        var options = {
+            uri: 'https://api.github.com/repos/marou95/ProjectNodeJS/commits',
+            headers: {
+                'User-Agent': 'Request-Promise'
+            },
+            json: true // Automatically parses the JSON string in the response
+        };
+        
+        rp(options)
+            .then(function (result) {
+                //on récupère en variable les informations de l'objet JSON(url) que l'on voudra afficher
+                 for(i=0; i<result.length; i++){
+                    
+                    authorName = result[i].commit.author.name;
+                    authorEmail= result[i].commit.author.email;
+                    dateCommit = result[i].commit.author.date;
+                    commitMessage = result[i].commit.message;
+    
+                 }
+                res.render("gitCommit.ejs",{project: project, 
+                    result:result,
+                    authorName: authorName,  
+                    authorEmail:authorEmail,
+                    dateCommit: dateCommit,
+                    commitMessage: commitMessage });
+        
+            })
+            .catch(function (err) {
+                // API call failed...
+                console.log(err)
+                res.status(500).send();
+            });
+    
     });
 });
 
